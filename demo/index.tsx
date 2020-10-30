e00362f254e3db98b10b0278935ab67e950af8f6
Matthias Giger <i@matthiasgiger.com>
2020-10-30 17:56:40 +0100

    feat(markup): rendering markup of nested object recursively

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,5 +1,36 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Named } from 'konfi'
+import { Konfi, Type } from 'konfi'
 
-render(<p>test</p>, document.body)
+const data = {
+  someValue: 5,
+  nested: {
+    anotherValue: 'test',
+    deeplyNested: {
+      oneMoreValue: 3.33,
+    },
+  },
+}
+
+const schema = {
+  someValue: {
+    type: Type.number,
+  },
+  nested: {
+    anotherValue: {
+      type: Type.string,
+    },
+    deeplyNested: {
+      oneMoreValue: {
+        type: Type.number,
+      },
+    },
+  },
+}
+
+render(
+  <div>
+    <Konfi schema={schema} data={data} />
+  </div>,
+  document.body
+)
--- markup.tsx
+++ markup.tsx
@@ -1,5 +1,47 @@
 import React from 'react'
+import { Schema } from './index'
 
-export const markup = (schema, data) => {
-  return <div className="konfi">hello</div>
+const isNested = (data: any) =>
+  !data || typeof data !== 'object' || Array.isArray(data)
+
+// Recursively render current object level.
+const Level = ({
+  data,
+  indentation = 1,
+}: {
+  data: any
+  indentation?: number
+}) => {
+  // Recursion ending condition.
+  if (isNested(data)) {
+    return null
+  }
+
+  return (
+    <div style={{ marginLeft: indentation * 20 }}>
+      {Object.keys(data).map((key) => {
+        const next = data[key]
+        const nested = isNested(next)
+        return (
+          <div key={key}>
+            <p>
+              {key}: {nested ? data[key] : '{'}
+            </p>
+            <Level data={data[key]} indentation={indentation + 1} />
+            <p>{nested ? '' : '}'}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export const markup = (schema: Schema, data: any) => {
+  return (
+    <div className="konfi" style={{ fontFamily: 'monospace' }}>
+      <p>{'{'}</p>
+      <Level data={data} />
+      <p>{'}'}</p>
+    </div>
+  )
 }
